6e3baf917af739b541d0f03e42138b4773bdc32c
ttoasted <gomezbenjamin117@gmail.com>
2026-08-02 22:19:40 -0600

    Update rainetti.js

--- rainetti.js
+++ rainetti.js
@@ -19,7 +19,7 @@ function randomInRange(min, max) {
     origin: {
       x: Math.random(),
       // since particles fall down, skew start toward the top
-      y: (Math.random() * skew) - 0.0
+      y: (Math.random() * skew) - 0.2
     },
     colors: ['#ccff33'],
     shapes: ['square'],
